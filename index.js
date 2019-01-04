5613463f2f28fbd50a62a618070cd64c2b949055
zaid85j <zaid85j@gmail.com>
2019-01-04 12:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+class Driver{
+constructor(name,startDate){
+  this.name=name;
+  // date should be formated  yy:mm:dd
+  this.startDate=new Date(startDate);
+}
+ yearsExperienceFromBeginningOf(year){
+  let yearD = new Date(year + "")
+  return yearD.getFullYear()-this.startDate.getFullYear()
+}
+}
+class Route{
+  constructor(horizontal,vertical){
+    this.horizontal=horizontal
+    this.vertical=vertical
+}
+blocksTravelled(horizontal,vertical){
+  
+}
+  
+}
